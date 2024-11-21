c0bda6b5a9579bacd253d30b96fbe6961e109b44
Arya <xue.peng@dfrobot.com>
2024-11-21 11:23:48 +0800

    更新网关实时接受函数和节点实时接收函数

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+LoRaWAN.onP2PReceived(function (sourceaddr, data, snr) {
+    serial.writeLine("source addr: " + sourceaddr + ", data:" + data + ", snr:" + snr)
+})
 serial.redirect(
 SerialPin.P12,
 SerialPin.P13,
 BaudRate.BaudRate115200
 )
 basic.pause(5000)
+LoRaWAN.initialize()
+LoRaWAN.connectNodeAdvanced868(
+1,
+LoRaFreq868.EU868_8681,
+LoRaEirp868.DBM16,
+LoRaSF868.SF12
+)
+LoRaWAN.connectNode()
